b47689db414f23b27ea3c47a606adcb5a16fdcde
cainamicael <cainamicaeloficial@gmail.com>
2023-08-21 16:45:32 -0300

    cors e primeira página dinâmica

--- musicas/src/main/java/io/github/cainamicael/musicas/config/WebConfig.java
+++ musicas/src/main/java/io/github/cainamicael/musicas/config/WebConfig.java
@@ -1,25 +1,19 @@
 package io.github.cainamicael.musicas.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
 
-	@Value("${cors.origins}")
-	private String corsOrigins;
-	
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedMethods("*").allowedOrigins(corsOrigins);
-			}
-		};
-	}
-	
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://127.0.0.1:5500") // Adicione as origens permitidas aqui
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("*");
+    }
 }
